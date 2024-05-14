16b2a871749c8befc9b6f4f8f7ba53e49c018c22
dailydoseofdevops <dosedevops@gmail.com>
2024-05-13 21:27:14 -0400

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+class Calculator:
+  def add(self, a, b):
+    return a + b
+
+  def subtract(self, a, b):
+    return a - b
+
+  def multiply(self, a, b):
+    return a * b
+
+  def divide(self, a, b):
+    if b == 0:
+      raise ValueError("Can't divide by zero!")
+    return a / b
