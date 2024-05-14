46d2c781f765215bb87bbf6202de29f33e850ec8
dailydoseofdevops <dosedevops@gmail.com>
2024-05-13 21:31:09 -0400

    Create run_calculator.py

---
+++ run_calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from calculator import Calculator
+
+calc = Calculator()
+print(calc.add(4, 5))  # prints: 9
+print(calc.subtract(10, 5))  # prints: 5
+print(calc.multiply(2, 3))  # prints: 6
+print(calc.divide(10, 2))  # prints: 5.0
