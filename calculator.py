27e0737e9559b0c982c6af0012d02d3151289607
dailydoseofdevops <rich@Luna.local>
2024-05-14 15:31:25 -0400

    Formatting changes

--- calculator.py
+++ calculator.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 class Calculator:
-  def add(self, a, b):
-    return a + b
+    def add(self, a, b):
+        return a + b
 
-  def subtract(self, a, b):
-    return a - b
+    def subtract(self, a, b):
+        return a - b
 
-  def multiply(self, a, b):
-    return a * b
+    def multiply(self, a, b):
+        return a * b
 
-  def divide(self, a, b):
-    if b == 0:
-      raise ValueError("Can't divide by zero!")
-    return a / b
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Can't divide by zero!")
+        return a / b
 
-  def power(self, num1, num2):
-    return num1 ** num2
+    def power(self, num1, num2):
+        return num1**num2
--- test_calculator.py
+++ test_calculator.py
@@ -2,28 +2,30 @@
 import unittest
 from calculator import Calculator
 
+
 class TestCalculator(unittest.TestCase):
-  def setUp(self):
-    self.calculator = Calculator()
+    def setUp(self):
+        self.calculator = Calculator()
 
-  def test_add(self):
-    self.assertEqual(self.calculator.add(2, 2), 4)
+    def test_add(self):
+        self.assertEqual(self.calculator.add(2, 2), 4)
 
-  def test_subtract(self):
-    self.assertEqual(self.calculator.subtract(2, 2), 0)
+    def test_subtract(self):
+        self.assertEqual(self.calculator.subtract(2, 2), 0)
 
-  def test_multiply(self):
-    self.assertEqual(self.calculator.multiply(2, 2), 4)
+    def test_multiply(self):
+        self.assertEqual(self.calculator.multiply(2, 2), 4)
 
-  def test_divide(self):
-    self.assertEqual(self.calculator.divide(4, 2), 2)
+    def test_divide(self):
+        self.assertEqual(self.calculator.divide(4, 2), 2)
 
-  def test_power(self):
-    self.assertEqual(self.calculator.power(2,8), 256)
+    def test_power(self):
+        self.assertEqual(self.calculator.power(2, 8), 256)
 
-  def test_divide_by_zero(self):
-    with self.assertRaises(ValueError):
-      self.calculator.divide(4, 0)
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            self.calculator.divide(4, 0)
 
-if __name__ == '__main__':
-  unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
